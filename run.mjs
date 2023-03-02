70bfa6cb0f7a6773d9a801f195ab730d2a413182
Carter Hesterman <hestermancarter@gmail.com>
2023-03-01 19:19:55 -0700

    init

---
+++ run.mjs
@@ -0,0 +1,85 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import decompress from "decompress";
+import request from "superagent";
+const BASE_URL = "https://minecraft.azureedge.net/bin-win/"; // https://minecraft.azureedge.net/bin-win/bedrock-server-1.19.63.01.zip bin-win can be swapped out for bin-linux for linux servers
+const outputFilename = `server-${new Date().getTime()}.zip`;
+const BROWSER_PAGE = "https://www.minecraft.net/en-us/download/server/bedrock";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const run = async () => {
+  const html = await getHTML();
+  const downloadUrl = extractDownloadURL(html);
+  const zipLocation = `${__dirname}/${outputFilename}`;
+  try {
+    await downloadZip(downloadUrl, zipLocation);
+    await extractZip(zipLocation);
+    await copyFiles();
+  } finally {
+    await cleanup(zipLocation);
+  }
+};
+
+const cleanup = async (zipLocation) => {
+  // delete zip file and folder if they exist
+};
+
+const copyFiles = async (targetPath) => {
+  // Delete server.properties from zip
+  // Copy and overwrite all files in targetpath
+};
+
+const extractZip = async (zipLocation) => {
+  console.log("Beginning extraction");
+  await decompress(zipLocation, zipLocation.replace(".zip", ""));
+  console.log("Extraction complete");
+};
+
+const downloadZip = async (url, targetPath) => {
+  return new Promise(async (resolve, reject) => {
+    request
+      .get(url)
+      .on("error", reject)
+      .pipe(fs.createWriteStream(targetPath))
+      .on("finish", () => {
+        console.log("Download complete");
+        resolve();
+      });
+  });
+};
+
+const extractDownloadURL = (html) => {
+  const regex = /https:\/\/minecraft.azureedge\.net\/bin-win\/.*\.zip/gm;
+  let m;
+  let downloadURL;
+
+  while ((m = regex.exec(html)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+      downloadURL = match;
+    });
+  }
+
+  if (!downloadURL) {
+    throw new Error(
+      `Unable to find matching download url at ${BROWSER_PAGE} using regex: ${regex}`
+    );
+  }
+
+  return downloadURL;
+};
+
+const getHTML = async () => {
+  const res = await fetch(BROWSER_PAGE);
+  const html = await res.text();
+  return html;
+};
+
+run();
